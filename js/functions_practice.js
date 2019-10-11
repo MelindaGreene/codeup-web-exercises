10e43146a8896bdcf6ee058e71a6f70c918623ef
MelindaGreene <Melindagreene711@gmail.com>
2019-10-11 08:33:24 -0500

    pratice

--- js/functions_practice.js
+++ js/functions_practice.js
@@ -7,3 +7,29 @@ function addition (x, y){
 function subtraction (x,y){
     return x- y;
 }
+
+
+function boolean(x){
+    return boolean("boolean");
+}
+
+function string(input){
+    return typeof(input == "boolean")
+}
+
+function numeric(input){
+    if (typeof input === "number") {
+        return true
+    }else {
+        return false
+    }
+}
+
+
+function isEven(input){
+    if(input % 2 === 0){
+        return true
+    } else {
+    return false
+}
+}
